705bea1c656644f6f8e223e9fa75b90dce1213a9
Arquam Ejaz <arquam.ejaz@gmail.com>
2022-04-15 02:59:01 +0530

    updated UI

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -17636,7 +17636,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40813" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42451" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
